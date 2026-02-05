ef0cca6275a525b453f2b2d4f0f234c6b3887f06
Plydy <myroveikask@gmail.com>
2026-02-05 06:15:30 +0100

    fix: import api_view

--- front/accounts/views.py
+++ front/accounts/views.py
@@ -1,10 +1,13 @@
 import os
-from django.contrib.auth import get_user_model
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, permission_classes
+
+from django.contrib.auth import get_user_model
 
 from .models import Patient
 from .serializers import PatientSerializer, MeSerializer, RegisterSerializer
